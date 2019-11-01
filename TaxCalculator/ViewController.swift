6249c240a00a75f88d491949540936575fa1fdff
Rori Ash <aash@lcs.on.ca>
2019-11-01 11:23:10 -0400

    Added a guard statement for the name and converted string data type to double

--- TaxCalculator/ViewController.swift
+++ TaxCalculator/ViewController.swift
@@ -13,7 +13,17 @@ class ViewController: UIViewController {
     // Mark: Properties
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var grossIncome: UITextField!
+    @IBOutlet weak var federalTaxOwing: UILabel!
+    @IBOutlet weak var effectiveTaxRate: UILabel!
     
+    var tax1 = 0.0
+    var tax2 = 0.0
+    var tax3 = 0.0
+    var tax4 = 0.0
+    var tax5 = 0.0
+    
+    
+    // Mark: Initializers
     
     // Mark: Methods
     override func viewDidLoad() {
@@ -23,8 +33,33 @@ class ViewController: UIViewController {
     
     // Button to calculate tax owing
     @IBAction func taxOwing(_ sender: Any) {
+    
+        // Get information from user
+        guard let user = name.text else  {
+            federalTaxOwing.text = "Please enter a name."
+        return
+        }
+        
+        let grossIncomeAsString = grossIncome.text!
+        
+        // Convert string data type to double (to calculate)
+        let grossIncomeAsDouble = Double(grossIncomeAsString)
+        
+        
+        // Calculs
+        switch grossIncomeAsDouble {
+            
+            
+            
+        }
+        
+        
+        
+        
+        // Report the information to the user
+
     }
     
-
+    
 }
 
