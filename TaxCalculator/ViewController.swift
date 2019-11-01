3679b92f3061e5b0fbf83c50120fd5e3e2967f6f
Rori Ash <aash@lcs.on.ca>
2019-11-01 10:21:40 -0400

    Initial Commit

---
+++ TaxCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaxCalculator
+//
+//  Created by Ash, Aurora on 2019-11-01.
+//  Copyright © 2019 Rori Ash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
