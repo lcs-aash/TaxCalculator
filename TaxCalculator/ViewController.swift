350ea0334670f46dfaaefeb28c7b331303a041eb
Rori Ash <aash@lcs.on.ca>
2019-11-01 10:34:32 -0400

    Connected variables and button to the view controller

--- TaxCalculator/ViewController.swift
+++ TaxCalculator/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Mark: Properties
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var grossIncome: UITextField!
+    
+    
+    // Mark: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // Button to calculate tax owing
+    @IBAction func taxOwing(_ sender: Any) {
+    }
+    
 
 }
 
