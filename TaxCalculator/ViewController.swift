d3447a4803d33389afa33e8adfb6e9195ae2f24a
Rori Ash <aash@lcs.on.ca>
2019-11-06 13:33:04 -0500

    Finished calculations

--- TaxCalculator/ViewController.swift
+++ TaxCalculator/ViewController.swift
@@ -9,19 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     // Mark: Properties
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var grossIncome: UITextField!
     @IBOutlet weak var federalTaxOwing: UILabel!
     @IBOutlet weak var effectiveTaxRate: UILabel!
-    
-    var tax1 = 0.0
-    var tax2 = 0.0
-    var tax3 = 0.0
-    var tax4 = 0.0
-    var tax5 = 0.0
-    
     
     // Mark: Initializers
     
@@ -33,33 +26,73 @@ class ViewController: UIViewController {
     
     // Button to calculate tax owing
     @IBAction func taxOwing(_ sender: Any) {
-    
+        
         // Get information from user
         guard let user = name.text else  {
             federalTaxOwing.text = "Please enter a name."
-        return
+            return
+            
+            guard let user = grossIncome.text else {
+                effectiveTaxRate.text = "Please enter a gross income in dollars."
+                return
+            }
         }
         
         let grossIncomeAsString = grossIncome.text!
         
         // Convert string data type to double (to calculate)
         let grossIncomeAsDouble = Double(grossIncomeAsString)
+        let grossIncomeAsInt = Int (grossIncomeAsDouble )
+        
+        // Variables for different gross incomes 
+        var tax1 = 0.0
+        var tax2 = 0.0
+        var tax3 = 0.0
+        var tax4 = 0.0
+        var tax5 = 0.0
         
         
-        // Calculs
-        switch grossIncomeAsDouble {
+        // Calculations
+        switch grossIncomeAsInt {
+        case 210472...:
+            tax5 = (grossIncomeAsInt - 210372) * 0.33
+            fallthrough
+        case 147667...210371:
+            tax4 = (grossIncomeAsInt > 62704) * 0.29
+        else tax4 = (grossIncomeAsInt - 62704) * 0.29
+            fallthrough
+        case 95259...147667:
+            tax3 = (grossIncomeAsInt > 52408) * 0.26
+        else tax3 = (grossIncomeAsInt - 52408) * 0.26
+            fallthrough
+        case 47630...95259:
+            tax2 = (grossIncomeAsInt > 47629) * 0.205
+        else tax2 = (grossIncomeAsInt - 47629) * 0.205
+            fallthrough
+        case ...47630:
+            tax1 = (grossIncomeAsInt > 47630)
+        default:
+            grossIncomeAsInt * 0.15
             
             
+            //Total tax added together
+            let totalTax = tax1 + tax2 + tax3 + tax4 + tax5
+            let taxRate = totalTax/grossIncomeAsDouble! * 100
             
+            //Send results to the view
+            federalTaxOwing.text = "Your federal tax is \(totalTax)"
+            
+            effectiveTaxRate.text = "Your effective tax rate is \(taxRate)"
         }
         
         
         
         
-        // Report the information to the user
-
+        
+        
     }
     
     
+    
+    
 }
-
